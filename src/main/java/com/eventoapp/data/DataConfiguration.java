5befe252b7dd0ab6843f144357145a8d7461bd0b
anderson-lda <anderson.lima.araujo@hotmail.com>
2021-08-12 15:16:06 -0300

    with mysql connection

---
+++ src/main/java/com/eventoapp/data/DataConfiguration.java
@@ -0,0 +1,36 @@
+package com.eventoapp.data;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_boot_1"); //necessario criar o database spring_boot_1 primeiro
+        dataSource.setUsername("developer");
+        dataSource.setPassword("12345");
+        return dataSource;
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect"); //ATUALIZEI aqui pra 5Dialect
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+	
+}
